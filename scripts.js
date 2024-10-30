52f8aa7429676cdd77ab2d67326c56f8ce021efb
tomazleo <tomazleozz@gmail.com>
2024-10-29 22:31:28 -0300

    evolução no scripts com alteração no css/html

--- scripts.js
+++ scripts.js
@@ -2,8 +2,21 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertVelue(){
 const inputCurrencyValue = document.querySelector(".input-currency").value
+const valorConvercao = document.querySelector(".valor-convercao")
+const valorConvertido = document.querySelector(".valor-convertido")
 const dolarToday = 5.25
 const convertValue = inputCurrencyValue / dolarToday
+
+valorConvercao.innerHTML = new Intl.NumberFormat("pt-br", {
+style: "currency",
+currency: "BRL"
+}).format(inputCurrencyValue)
+
+valorConvertido.innerHTML = new Intl.NumberFormat("en-us", {
+    style: "currency",
+    currency: "USD"
+    }).format(convertValue)
+
 console.log(convertValue)
 
 }
